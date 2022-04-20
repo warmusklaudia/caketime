0a2130e08cb831e01155ade9746c139dcb1914ef
KlaudiaWs <klaudia.warmus@student.howest.be>
2022-04-20 16:54:53 +0200

    added: ignoreLogs

--- App.tsx
+++ App.tsx
@@ -9,8 +9,12 @@ import {
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { MyStack } from './screens'
+import { LogBox } from 'react-native'
 
 export default function App() {
+  LogBox.ignoreLogs([
+    `AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage`,
+  ])
   let [fontsLoaded] = useFonts({
     DancingScript_400Regular,
     DancingScript_500Medium,
